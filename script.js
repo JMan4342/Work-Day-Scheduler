30b0790c5175dcd0c2fe8b7531cbe533a41c9157
Joseph Manning <jgmanning3@yahoo.com>
2021-06-09 09:13:55 -0700

    Color code calendar for past, present, future

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-var now = moment();
+// var now = moment();
+// var currentDay = document.querySelector("#currentDay");
+
+const currentHour = +moment().format("H");
+
+$(".time-block").each(function(){
+    const hour = +$(this).attr("id").split("-").pop();
+
+    if (currentHour === hour){
+        $(this).addClass("present");
+    } else if (currentHour < hour) {
+        $(this).addClass("future");
+    } else {
+        $(this).addClass("past");
+    }
+
+})
